1df4ea7b9bd9a50db18502fb77146b389ea505ab
Andrei Suciu <andreisuciu2005@yahoo.com>
2023-03-26 10:46:46 +0300

    Handlebars trial

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,28 @@ const Article = require('../models/Article');
 
 // Homepage Route
 router.get('/', async (req, res) => {
-  try {
-    const articles = await Article.find({ user_id: req.session.user._id }).sort(
-      { updatedAt: -1 }
-    );
-    res.render('index', {
-      title: 'Overview',
-      articles
-    });
-  } catch (err) {
-    res.render('index', {
-      title: 'Overview'
-    });
-  }
+	try {
+		const articles = await Article.find({ user_id: req.session.user._id }).sort(
+			{ updatedAt: -1 }
+		);
+		const mappedArticles = articles.map((a) => {
+			return {
+				_id,
+				title,
+				body,
+				author,
+				updatedAt,
+			};
+		});
+		res.render('index', {
+			title: 'Overview',
+			articles: mappedArticles,
+		});
+	} catch (err) {
+		res.render('index', {
+			title: 'Overview',
+		});
+	}
 });
 
 module.exports = router;
