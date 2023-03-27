bf9d5624217f161d2b55eb773e079112705171ee
Andrei Suciu <andreisuciu2005@yahoo.com>
2023-03-27 18:09:02 +0300

    Trial

--- config/db.js
+++ config/db.js
@@ -3,17 +3,18 @@ const config = require('./config');
 const mongo = `mongodb+srv://${config.MONGODB_USER}:${config.MONGODB_PASS}@${config.MONGODB_URI}`;
 // Connect to MongoDB
 const connectDB = async () => {
-  try {
-    await mongoose.set('useFindAndModify', false).connect(mongo, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true
-    });
-    console.log('MongoDB: Connected...');
-  } catch (err) {
-    console.log(`MongoDB: ${err.message}`);
-    process.exit(1);
-  }
+	try {
+		console.log(mongo);
+		await mongoose.set('useFindAndModify', false).connect(mongo, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useUnifiedTopology: true,
+		});
+		console.log('MongoDB: Connected...');
+	} catch (err) {
+		console.log(`MongoDB: ${err.message}`);
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
